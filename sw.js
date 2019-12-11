babb89b1fa4e05f56663b44040300b8096637eca
Heredia <MIHeredia@mednet.ucla.edu>
2019-12-11 09:47:53 -0800

    12/11/2019 0947

--- sw.js
+++ sw.js
@@ -30,5 +30,10 @@ self.addEventListener('activate', evt => {
 
 //fetch event
 self.addEventListener('fetch', evt => {
-  //  console.log('fetch event', evt);
+  //console.log('fetch event', evt);
+  evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request);
+      })
+  );
 });
