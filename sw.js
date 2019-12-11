52f039108d16f0a321c17ad966c15ccb33eb4701
Mauricio Heredia <mihq1990@gmail.com>
2019-12-10 23:03:53 -0800

    12/10/2019 11:03PM

--- sw.js
+++ sw.js
@@ -1,6 +1,26 @@
+const staticCacheName = 'site-static';
+const assets =[
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
 //Install service worker
 self.addEventListener('install', evt => {
-    console.log('service worker is installed');
+    //console.log('service worker is installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
 });
 
 //Activate Event
@@ -10,5 +30,5 @@ self.addEventListener('activate', evt => {
 
 //fetch event
 self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+  //  console.log('fetch event', evt);
 });
