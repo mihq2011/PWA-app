c2d85b85caec58a14bbc5a0e71c58a8f226144c5
Mauricio Heredia <mihq1990@gmail.com>
2019-12-19 13:29:03 -0800

    video 21

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const staticCacheName = 'site-static-v1';
-const dynamicCache = 'site-dynamic-v1'
+const dynamicCacheName = 'site-dynamic-v1';
 const assets =[
     '/',
     '/index.html',
@@ -10,7 +10,8 @@ const assets =[
     '/css/materialize.min.css',
     '/img/dish.png',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+    'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    '/pages/fallback.html'
 ];
 
 //Install service worker
@@ -31,7 +32,7 @@ self.addEventListener('activate', evt => {
         caches.keys().then(keys => {
             //console.log(keys); 
             return Promise.all(keys
-                .filter(key => key !== staticCacheName)
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
                 .map(key => caches.delete(key))
             )
         })
@@ -44,11 +45,15 @@ self.addEventListener('fetch', evt => {
   evt.respondWith(
       caches.match(evt.request).then(cacheRes => {
         return cacheRes || fetch(evt.request).then(fetchRes => {
-            return caches.open(dynamicCache).then(cache => {
+            return caches.open(dynamicCacheName).then(cache => {
                 cache.put(evt.request.url, fetchRes.clone());
                 return fetchRes;
             })
         });
-      })
+      }).catch(() => {
+      if(evt.request.url.indexOf('.html') > -1) { 
+      return caches.match('/pages/fallback.html');
+      }
+    })
   );
 });
